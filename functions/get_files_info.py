1bd1d2924c47ee2dbac358dd0c98531e1f24cde2
EentErt <nate.giff@gmail.com>
2025-06-12 15:11:42 -0700

    adding os commands to get_files_info in progress

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,7 +1,7 @@
 import os
 
 def get_files_info(working_directory, directory=None):
-    if directory not in working_directory:
+    if directory not in os.listdir(working_directory):
         return f"Error: Cannot list {directory} as it is outside the permitted working directory"
     if not os.path.isdir(directory):
         return f'Error: "{directory}" is not a directory'
--- tests.py
+++ tests.py
@@ -1,10 +1,9 @@
-import unittest
 from functions.get_files_info import get_files_info
 
 def main():
-    get_files_info('calculator', '.')
-    get_files_info('calculator', 'pkg')
-    get_files_info('calculator', '/bin')
-    get_files_info('calculator', '../')
+    print(get_files_info('calculator', '.'))
+    print(get_files_info('calculator', 'pkg'))
+    print(get_files_info('calculator', '/bin'))
+    print(get_files_info('calculator', '../'))
 
 main()
